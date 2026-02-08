82e82f836ed9fa2e31f6329379a7f81a5c34cb8d
Louise Mendiburu <louisemendiburu@gmail.com>
2026-02-08 15:21:51 +0100

    feat: init shadcn/ui

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
@@ -36,5 +39,10 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
