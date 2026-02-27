f1dd9d8bb2f12fd93bd0b33aeb14d5c4f026967a
Louise Mendiburu <louisemendiburu@gmail.com>
2026-02-27 11:04:03 +0100

    fix: logo pro source fix

--- front/src/components/layout/ProLayout.tsx
+++ front/src/components/layout/ProLayout.tsx
@@ -6,7 +6,7 @@ export default function ProLayout() {
     <div className="min-h-screen flex flex-col">
       <header className="pro-header">
         <div className="pro-header-inner">
-          <img src="/logo-pro.svg" alt="logo pro" className="pro-header-logo" />
+          <img src="/logo/logo-pro.svg" alt="logo pro" className="pro-header-logo" />
         </div>
       </header>
       <main className="flex-1 pt-14 flex justify-center">
